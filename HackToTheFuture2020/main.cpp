7f5e8dcfcd3ce0cccebfaf7c75a0619346ef79d4
ao1415 <ao14159265@gmail.com>
2019-11-02 20:54:18 +0900

    ガチャ導入

--- HackToTheFuture2020/main.cpp
+++ HackToTheFuture2020/main.cpp
@@ -92,6 +92,86 @@ public:
 
 };
 
+class MilliSecTimer {
+private:
+
+	std::chrono::time_point<std::chrono::high_resolution_clock> s;
+	unsigned long long int startCycle = 0;
+	long long time = 0;
+
+	const double CyclePerMilliSec = 2794000.0;
+
+#ifndef _MSC_VER
+	unsigned long long int getCycle() const {
+		unsigned int low, high;
+		__asm__ volatile ("rdtsc" : "=a" (low), "=d" (high));
+		return ((unsigned long long int)low) | ((unsigned long long int)high << 32);
+	}
+#endif // _MSC_VER
+
+public:
+
+	/// <summary>
+	/// コンストラクタ
+	/// </summary>
+	MilliSecTimer() = default;
+	/// <summary>
+	/// コンストラクタ
+	/// </summary>
+	/// <param name="_time">設定時間(ミリ秒)</param>
+	MilliSecTimer(const std::chrono::milliseconds& _time) noexcept { time = _time.count(); }
+
+	/// <summary>
+	/// 時間を設定する
+	/// </summary>
+	/// <param name="_time">設定時間(ミリ秒)</param>
+	void set(const std::chrono::milliseconds& _time) noexcept { time = _time.count(); }
+
+	/// <summary>
+	/// タイマーを開始させる
+	/// </summary>
+	void start() noexcept {
+#ifdef _MSC_VER
+		s = std::chrono::high_resolution_clock::now();
+#else
+		startCycle = getCycle();
+#endif // _MSC_VER
+	}
+
+	/// <summary>
+	/// 設定時間経過したかを得る
+	/// </summary>
+	/// <returns>経過していれば true, それ以外は false</returns>
+	inline const bool check() const noexcept {
+#ifdef _MSC_VER
+		const auto e = std::chrono::high_resolution_clock::now();
+		return std::chrono::duration_cast<std::chrono::milliseconds>(e - s).count() >= time;
+#else
+		return (getCycle() - startCycle) / CyclePerMilliSec >= time;
+#endif // _MSC_VER
+	}
+
+	/// <summary>
+	/// 設定時間経過したかを得る
+	/// </summary>
+	/// <returns>経過していれば true, それ以外は false</returns>
+	operator bool() const noexcept { return check(); }
+
+	/// <summary>
+	/// 経過時間を取得する(ミリ秒)
+	/// </summary>
+	/// <returns>計測時間(ミリ秒)</returns>
+	inline const long long interval() const noexcept {
+#ifdef _MSC_VER
+		const auto e = std::chrono::high_resolution_clock::now();
+		return std::chrono::duration_cast<std::chrono::milliseconds>(e - s).count();
+#else
+		return static_cast<long long int>((getCycle() - startCycle) / CyclePerMilliSec);
+#endif // _MSC_VER
+	}
+
+};
+
 constexpr int N = 40;
 constexpr int M = 100;
 constexpr int B = 300;
@@ -299,9 +379,16 @@ private:
 
 				if (passTable[p] != Panel::None)
 				{
+					/*
 					if (maxRange < stepTable[p])
 					{
 						maxRange = stepTable[p];
+						maxPos = p;
+					}
+					*/
+					if (maxRange < rangeTable[p])
+					{
+						maxRange = rangeTable[p];
 						maxPos = p;
 					}
 				}
@@ -596,7 +683,25 @@ public:
 			}
 		}
 
+		mt19937 randmt;
+		MilliSecTimer timer;
+		timer.set(chrono::milliseconds(2900));
 
+		timer.start();
+		while (!timer)
+		{
+			auto rangeSortRobots = iRobots;
+			shuffle(rangeSortRobots.begin(), rangeSortRobots.end(), randmt);
+			const auto answer = greedy(rangeSortRobots);
+			int score = Simulator(goal, field, iRobots, answer).getScore();
+			if (bestScore < score)
+			{
+				bestScore = score;
+				bestAnswer = answer;
+			}
+		}
+
+		cerr << bestScore << endl;
 
 		if (!bestAnswer.empty()) return bestAnswer;
 		return { Command{0,0,Panel::U} };
